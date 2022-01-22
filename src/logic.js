e37f7eded4ba3de0cdf2885633332346cd9cbaf3
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-01-22 18:56:31 +0530

    second  commit

--- src/logic.js
+++ src/logic.js
@@ -1,73 +1,121 @@
 function info() {
-    console.log("INFO")
-    const response = {
-        apiversion: "1",
-        author: "",
-        color: "#888888",
-        head: "default",
-        tail: "default"
-    }
-    return response
+  console.log("INFO");
+  const response = {
+    apiversion: "1",
+    author: "",
+    color: "#888888",
+    head: "default",
+    tail: "default",
+  };
+  return response;
 }
 
 function start(gameState) {
-    console.log(`${gameState.game.id} START`)
+  console.log(`${gameState.game.id} START`);
 }
 
 function end(gameState) {
-    console.log(`${gameState.game.id} END\n`)
+  console.log(`${gameState.game.id} END\n`);
 }
 
 function move(gameState) {
-    let possibleMoves = {
-        up: true,
-        down: true,
-        left: true,
-        right: true
+  const possibleMoves = ["up", "down", "left", "right"];
+
+  var SafeMoves = GetSafeMoves(gameState, possibleMoves);
+  console.log(SafeMoves);
+
+  //if the no of safe moves are greater than 1
+  if (SafeMoves) {
+    var moveChoosen = SafeMoves[Math.floor(Math.random() * SafeMoves.length())];
+  } else {
+    // if there are no other safe moves
+    moveChoosen =
+      possibleMoves[Math.floor(Math.random() * possibleMoves.length())];
+  }
+
+  //preparing the response
+  var response = {
+    move: moveChoosen,
+  };
+
+  //log the choosen move to make debuggin easier if you have to
+  console.log(`${gameState.game.id} MOVE ${gameState.turn}: ${response.move}`);
+
+  return response;
+}
+
+function GetSafeMoves(gameState, possibleMoves) {
+  var SafeMoves = [];
+  //this is the snake body
+  const OurSnakeBody = gameState["you"]["body"];
+  //checker head details
+  const SnakeHead = gameState["you"]["head"];
+
+  const getBoardDetails = gameState["board"];
+  //wall of the snake is at x != 11 or y != 11
+  // x !< 0 y !< 0
+
+  // its a fact that head is what is leading the snake to any direction so head.x < 11 and head.y < 11 shoul;d d0
+  possibleMoves.forEach((move) => {
+    var GetCoordinates = getNextCoordinatesOfTheMove(SnakeHead, move);
+    var SnakeTail = OurSnakeBody[OurSnakeBody.length - 1];
+    var SnakeBodyExceptTail = OurSnakeBody.slice(0, OurSnakeBody.length - 2);
+    //analyze the board and other snakes
+    if (
+      avoidHittingTheWalls(
+        GetCoordinates,
+        getBoardDetails["height"],
+        getBoardDetails["width"]
+      )
+    ) {
+      SafeMoves.push(move);
+    } else if (
+      OurSnakeBody.length > 1 &&
+      JSON.stringify(GetCoordinates) == JSON.stringify(SnakeTail) &&
+      CheckSnakeHeadNotInBody(GetCoordinates, SnakeBodyExceptTail)
+    ) {
+      SafeMoves.push(move);
     }
+  });
+  return SafeMoves;
+}
 
-    // Step 0: Don't let your Battlesnake move back on its own neck
-    const myHead = gameState.you.head
-    const myNeck = gameState.you.body[1]
-    if (myNeck.x < myHead.x) {
-        possibleMoves.left = false
-    } else if (myNeck.x > myHead.x) {
-        possibleMoves.right = false
-    } else if (myNeck.y < myHead.y) {
-        possibleMoves.down = false
-    } else if (myNeck.y > myHead.y) {
-        possibleMoves.up = false
-    }
+function avoidHittingTheWalls(HeadCoordinates, height, width) {
+  var result = true;
+  if (
+    HeadCoordinates.x < 0 ||
+    HeadCoordinates.y < 0 ||
+    HeadCoordinates.x >= width ||
+    HeadCoordinates.y >= height
+  ) {
+    result = false;
+  } else {
+    result = true;
+  }
 
-    // TODO: Step 1 - Don't hit walls.
-    // Use information in gameState to prevent your Battlesnake from moving beyond the boundaries of the board.
-    // const boardWidth = gameState.board.width
-    // const boardHeight = gameState.board.height
+  return result;
+}
 
-    // TODO: Step 2 - Don't hit yourself.
-    // Use information in gameState to prevent your Battlesnake from colliding with itself.
-    // const mybody = gameState.you.body
-
-    // TODO: Step 3 - Don't collide with others.
-    // Use information in gameState to prevent your Battlesnake from colliding with others.
-
-    // TODO: Step 4 - Find food.
-    // Use information in gameState to seek out and find food.
-
-    // Finally, choose a move from the available safe moves.
-    // TODO: Step 5 - Select a move to make based on strategy, rather than random.
-    const safeMoves = Object.keys(possibleMoves).filter(key => possibleMoves[key])
-    const response = {
-        move: safeMoves[Math.floor(Math.random() * safeMoves.length)],
-    }
-
-    console.log(`${gameState.game.id} MOVE ${gameState.turn}: ${response.move}`)
-    return response
+function getNextCoordinatesOfTheMove(SnakeHead, move) {
+  var futureHeadOfSnake = Object.assign({}, SnakeHead);
+  if (move == "up") {
+    futureHeadOfSnake.y = SnakeHead.y + 1;
+  }
+  if (move == "down") {
+    futureHeadOfSnake.y = SnakeHead.y - 1;
+  }
+  if (move == "left") {
+    futureHeadOfSnake.x = SnakeHead.x - 1;
+  }
+  if (move == "right") {
+    futureHeadOfSnake.x = SnakeHead.x + 1;
+  }
+  return futureHeadOfSnake;
 }
 
 module.exports = {
-    info: info,
-    start: start,
-    move: move,
-    end: end
-}
+  info: info,
+  start: start,
+  move: move,
+  end: end,
+};
